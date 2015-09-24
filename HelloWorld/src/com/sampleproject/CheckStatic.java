921e142d0c9442e9f8d019ef16fa44a9df9823cb
sidaxe <sidhant1992@gmail.com>
2015-09-24 14:34:46 +0530

    just commiting

---
+++ HelloWorld/src/com/sampleproject/CheckStatic.java
@@ -0,0 +1,16 @@
+package com.sampleproject;
+
+public class CheckStatic {
+	private int id;
+	
+	public CheckStatic(){
+		id=20;
+	}
+	public int plus5(){
+		return return10()+id+5;
+	}
+   public int return10(){
+	   int i=10;
+	   return i;
+   }
+}
---
+++ HelloWorld/src/com/sampleproject/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.sampleproject;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	
+		CheckStatic obj=new CheckStatic();
+		System.out.println(obj.plus5());
+	
+	
+	}
+	
+}

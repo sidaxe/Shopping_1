67e57de21951ab6d77ef43e203226a6eed795f2a
sidaxe <sidhant1992@gmail.com>
2015-10-11 00:47:17 +0530

    fixed

--- MyStylist/src/main/java/com/grabalook/rest/UserResource.java
+++ MyStylist/src/main/java/com/grabalook/rest/UserResource.java
@@ -1,14 +1,14 @@
 package com.grabalook.rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.grabalook.dao.CustomerDao;
-import com.grabalook.pojo.Customer;
+import com.grabalook.pojo.User;
 
 
 @Path("/users")
@@ -537,14 +537,15 @@ public class UserResource {
 		 
 		return Response.status(200).entity(output).build();
 	}
-	*/
+
 	@POST
 	@Path("/signin")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response signin(Customer customer){
-		System.out.println(customer.getEmailId());
+	public Response signin(User customer){
+		System.out.println(customer.getEmail());
 		return Response.status(200).build();	
 		
 	}
+}
 
 
